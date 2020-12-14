13284e0665078711cec62323bbaa6d985084a416
Crowdbotics <team@crowdbotics.com>
2020-12-14 11:54:41 +0000

    Update components

---
+++ src/features/BlankScreen015740/screens/blank.js
@@ -0,0 +1,47 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
+import { connect } from "react-redux"
+export class Blank extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
+  }
+  state = { Switch_4: true }
+  render = () => (
+    <View>
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <Switch
+        trackColor={{ false: "#C0CCDA", true: "#409EFF" }}
+        style={styles.Switch_4}
+        value={this.state.Switch_4}
+        onValueChange={nextChecked => this.setState({ Switch_4: nextChecked })}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  View_1: {},
+  Button_2: {},
+  Switch_4: { alignSelf: "flex-start" }
+})
+
+function mapStateToProps(state) {
+  return { state: state.apiReducer }
+}
+const actionCreators = {}
+export default connect(mapStateToProps, actionCreators)(Blank)
